46695b787af38e99d4eccc2c740023d5c67b960b
Miguel Ángel Tribaldos <mtribaldos@gmail.com>
2016-10-14 16:30:00 +0200

    For the moment, 'corr.R' is only capable of filtering the requested samples (above the threshold)

--- Programming_with_R/Week_2/Programming_Assignment/corr.R
+++ Programming_with_R/Week_2/Programming_Assignment/corr.R
@@ -3,13 +3,9 @@ corr <- function(directory, threshold = 0) {
     files <- list.files(directory, full.name = TRUE)
     file_data_list <- lapply(files, read.csv)
     alldata <- do.call(rbind, file_data_list)
-    
-    data <- alldata
-    complete_data <- data[complete.cases(data),]
  
-    frequency <- as.data.frame(table(complete_data$ID))
-    colnames(frequency) <- c("id", "nobs")
+    above_thr <- complete(directory)$nobs > threshold
 
-    frequency
+    data <- alldata[above_thr,]
 }
 
