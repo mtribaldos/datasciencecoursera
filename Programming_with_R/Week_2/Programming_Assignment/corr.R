3dd49d21c2bde8cb9196232ff44c2fe2165a01e3
Miguel Angel Tribaldos <mtribaldos@DebDev.magip.net>
2016-10-15 00:22:02 +0200

    Fixed how to get the filtered _data_ vector from the threshold vector

--- Programming_with_R/Week_2/Programming_Assignment/corr.R
+++ Programming_with_R/Week_2/Programming_Assignment/corr.R
@@ -3,9 +3,12 @@ corr <- function(directory, threshold = 0) {
     files <- list.files(directory, full.name = TRUE)
     file_data_list <- lapply(files, read.csv)
     alldata <- do.call(rbind, file_data_list)
- 
-    above_thr <- complete(directory)$nobs > threshold
 
-    data <- alldata[above_thr,]
+    compl <- complete(directory)
+    above_thr <- compl$id[compl$nobs > threshold]
+
+    data <- alldata[alldata$ID %in% above_thr,]
+    #aggregate(data, by=list(data$ID), FUN=function(x) cor(x["sulfate"], x["nitrate"]))
+    aggregate(data$sulfate, by=list(data$ID), FUN=function(x) mean(x, na.rm=TRUE))
 }
 
